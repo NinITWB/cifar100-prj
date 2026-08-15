720035e61df4fdc4e045f3d7256ea9e8f8281daf
Ninkuto <catphu703@gmail.com>
2026-08-15 10:31:25 +0700

    update:code

--- src/computer_vision/main.py
+++ src/computer_vision/main.py
@@ -1,2 +1,44 @@
 from models import SimpleCNN
 from models import SimpleMLP
+from torch import nn
+from data.data_manipulation import *
+import torch
+from tqdm import tqdm
+
+def train_cnn(cnn, features: torch.Tensor, labels, loss_func, optimizer):
+    cnn.train()
+    optimizer.zero_grad()
+    feat_return = cnn(features)
+    logits, representation = feat_return
+    loss_val = loss_func(logits, labels)
+
+    loss_val.backward()
+    optimizer.step()
+    return loss_val.item()
+
+
+def main():
+    cnn = SimpleCNN()
+
+    loss_func = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.SGD(
+        params=cnn.parameters(),
+        lr=0.01
+    )
+    epoch = 10
+    for i in range(epoch):
+        total_loss = 0.0
+        for idx, (features, labels) in tqdm(enumerate(train_loader)):
+            loss = train_cnn(cnn, features, labels, loss_func=loss_func, optimizer=optimizer)
+
+            total_loss += loss
+        average_loss = total_loss / len(train_loader)
+
+        print(
+            f"Epoch {i + 1}/{epoch} "
+            f"Loss: {average_loss:.4f}"
+        )
+
+if __name__ == "__main__":
+    main()
--- src/data/data_manipulation.py
+++ src/data/data_manipulation.py
@@ -4,17 +4,28 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 
-dataset = datasets.CIFAR100(
+train_dataset = datasets.CIFAR100(
     root="data",
     train=True,
     download=True,
     transform=ToTensor()
 )
 
-loader = DataLoader(
-    dataset=dataset,
-    batch_size=16,
-    shuffle=True
+test_dataset = datasets.CIFAR100(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
 )
 
-images, labels = next(iter(loader))
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=32,
+    shuffle=False
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=32,
+    shuffle=True
+)
